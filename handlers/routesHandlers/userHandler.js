1f0a5166e0a0f89729a5fd3834f3be3cad0a8c05
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2024-04-24 10:13:59 +0600

    authenticate update and delete user

--- handlers/routesHandlers/userHandler.js
+++ handlers/routesHandlers/userHandler.js
@@ -41,13 +41,13 @@ handler._users.get = (requestProperties, callback) => {
                     })
                 }else{
                     callback(403,{
-                        error: "unauthenticated user 2!"
+                        error: "unauthenticated user !"
                     })
                 }
             });
         }else{
             callback(403,{
-                error: "unauthenticated user 1!"
+                error: "unauthenticated user!"
             });
         }
     } else {
@@ -107,40 +107,55 @@ handler._users.put = (requestProperties, callback) => {
     const password = typeof (requestProperties.body.password) === "string" && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password : false;
 
     if (phone) {
-        if (firstName || lastName || password) {
-            lib.read('users', phone, (err, data) => {
-                if (err) {
-                    callback(404, {
-                        error: "user not found !"
-                    })
-                } else {
-                    const userData = parseJSON(data);
-                    if (firstName) {
-                        userData.firstName = firstName;
+        const tokenId = typeof (requestProperties.headers.tokenid) === "string" && requestProperties.headers.tokenid.trim().length === 20 ? requestProperties.headers.tokenid : false;
+        if(tokenId){
+            tokenHandler._token.verify(tokenId,phone,(token)=>{
+                if(token){
+                    if (firstName || lastName || password) {
+                        lib.read('users', phone, (err, data) => {
+                            if (err) {
+                                callback(404, {
+                                    error: "user not found !"
+                                })
+                            } else {
+                                const userData = parseJSON(data);
+                                if (firstName) {
+                                    userData.firstName = firstName;
+                                }
+                                if (lastName) {
+                                    userData.lastName = lastName;
+                                }
+                                if (password) {
+                                    userData.password = hashPassword(password);
+                                }
+                                lib.update("users", phone, userData, (err) => {
+                                    if (err) {
+                                        callback(400, {
+                                            error: "failed to update data !"
+                                        })
+                                    } else {
+                                        callback(200, {
+                                            message: "user updated successfully."
+                                        })
+                                    }
+                                })
+                            }
+                        })
+                    } else {
+                        callback(500, {
+                            error: "Invalid request"
+                        })
                     }
-                    if (lastName) {
-                        userData.lastName = lastName;
-                    }
-                    if (password) {
-                        userData.password = hashPassword(password);
-                    }
-                    lib.update("users", phone, userData, (err) => {
-                        if (err) {
-                            callback(400, {
-                                error: "failed to update data !"
-                            })
-                        } else {
-                            callback(200, {
-                                message: "user updated successfully."
-                            })
-                        }
+                }else{
+                    callback(403,{
+                        error: "unauthenticated user!"
                     })
                 }
-            })
-        } else {
-            callback(500, {
-                error: "Invalid request"
-            })
+            });
+        }else{
+            callback(403,{
+                error: "unauthenticated user!"
+            });
         }
     } else {
         callback(400, {
@@ -151,28 +166,47 @@ handler._users.put = (requestProperties, callback) => {
 }
 handler._users.delete = (requestProperties, callback) => {
     const phone = typeof (requestProperties.queryStringObject.phone) === "string" && requestProperties.queryStringObject.phone.trim().length === 11 ? requestProperties.queryStringObject.phone : false;
+
     if (phone) {
-        lib.read('users', phone, (err, u) => {
-            const user = parseJSON(u);
-            delete user.password;
-            if (err && !u) {
-                callback(404, {
-                    error: "user not found"
-                })
-            } else {
-                lib.delete("users", phone, (err) => {
-                    if (err) {
-                        callback(202, {
-                            message: "delete successful"
-                        })
-                    } else {
-                        callback(500, {
-                            error: "failed to delete"
-                        })
-                    }
-                })
-            }
-        })
+        const tokenId = typeof (requestProperties.headers.tokenid) === "string" && requestProperties.headers.tokenid.trim().length === 20 ? requestProperties.headers.tokenid : false;
+        
+        if(tokenId){
+            tokenHandler._token.verify(tokenId,phone,(token)=>{
+                if(token){
+                    lib.read('users', phone, (err, u) => {
+                        const user = parseJSON(u);
+                        delete user.password;
+                        if (err && !u) {
+                            callback(404, {
+                                error: "user not found"
+                            })
+                        } else {
+                            lib.delete("users", phone, (err) => {
+                                if (err) {
+                                    callback(202, {
+                                        message: "delete successful"
+                                    })
+                                } else {
+                                    callback(500, {
+                                        error: "failed to delete"
+                                    })
+                                }
+                            })
+                        }
+                    })
+                }else{
+                    callback(403,{
+                        error: "unauthenticated user!"
+                    })
+                }
+            });
+        }else{
+            callback(403,{
+                error: "unauthenticated user!"
+            });
+        }
+        //look up the user
+        
     } else {
         callback(500, {
             error: "Invalid phone number."
