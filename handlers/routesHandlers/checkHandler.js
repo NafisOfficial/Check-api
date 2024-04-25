8ae92d36166260d4054d3cb91fa25caf6d4b1232
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2024-04-25 18:33:33 +0600

    user checks post method done

--- handlers/routesHandlers/checkHandler.js
+++ handlers/routesHandlers/checkHandler.js
@@ -8,7 +8,7 @@ const {maxChecks} = require('../../helpers/environments');
 // module scaffolding 
 const handler = {};
 
-handler.userHandler = (requestProperties, callback) => {
+handler.checkHandler = (requestProperties, callback) => {
     const acceptedMethod = ['get', 'post', 'put', 'delete'];
 
     if (acceptedMethod.indexOf(requestProperties.method) > -1) {
@@ -29,36 +29,42 @@ handler._check.get = (requestProperties, callback) => {
 }
 
 handler._check.post = (requestProperties, callback) => {
-
+    //valided all parameters
     const protocol = typeof (requestProperties.body.protocol) === "string" && ["http", "https"].indexOf(requestProperties.body.protocol) > -1 ? requestProperties.body.protocol : false;
 
     const url = typeof (requestProperties.body.url) === "string" && requestProperties.body.url.trim().length > 0 ? requestProperties.body.url : false;
 
-    const method = typeof (requestProperties.body.method) === "string" && ["get", "post", "put", "delete"].indexOf(requestProperties.body.method) > -1 ? requestProperties.body.method : false;
+    const method = typeof (requestProperties.body.method) === "string" && ["GET", "POST", "PUT", "DELETE"].indexOf(requestProperties.body.method.toUpperCase()) > -1 ? requestProperties.body.method : false;
 
     const successCode = typeof (requestProperties.body.successCode) === "object" && Array.isArray(requestProperties.body.successCode) === true ? requestProperties.body.successCode : false;
 
     const timeOutSeconds = typeof (requestProperties.body.timeOutSeconds) === "number" && requestProperties.body.timeOutSeconds % 1 === 0 && requestProperties.body.timeOutSeconds > 0 && requestProperties.body.timeOutSeconds < 6 ? requestProperties.body.timeOutSeconds : false;
 
+    // valid all parameters comes true
     if(protocol && url && method && successCode && timeOutSeconds){
+        //get the tokenid from header
         const tokenId = typeof (requestProperties.headers.tokenid) === "string" && requestProperties.headers.tokenid.trim().length === 20 ? requestProperties.headers.tokenid : false;
-
         if(tokenId){
+            //read the tokens from tokens folder
             lib.read('tokens',tokenId,(error,token)=>{
-                if(error){
+                if(error && !token){
                     callback(403,{
-                        error: "unauthenticated user !"
+                        error: "unauthenticated user 2!"
                     }) 
                 }else{
+                    // get phone number from tokens
                     const phone = parseJSON(token).phone;
                     if(phone){
+                        // get related user from users folder using phone numbers of tokens
                         lib.read('users',phone,(error,user)=>{
                             if(error && !user){
                                 callback(404,{
                                     error: "user not found !"
                                 })
                             }else{
-                                tokenHandler._token.verify(tokenId,user.phone,(verifiedUser)=>{
+                                // verify the corresponding tokenid and phone number
+                                const userPhone = parseJSON(user).phone
+                                tokenHandler._token.verify(tokenId,userPhone,(verifiedUser)=>{
                                     if(verifiedUser){
                                         const userObject = parseJSON(user);
                                         const userChecks = typeof(userObject.checks) === "object" && userObject.checks instanceof Array ? userObject.checks : [];
@@ -73,13 +79,26 @@ handler._check.post = (requestProperties, callback) => {
                                                 successCode,
                                                 timeOutSeconds
                                             }
+                                            //create checks in checks folder
                                             lib.create('checks',checkId,checkObject,(error)=>{
                                                 if(error){
                                                     callback(500,{
                                                         error: "there was a problem in server side."
                                                     });
                                                 }else{
-                                                    
+                                                    //update the new users data
+                                                    userObject.checks = userChecks;
+                                                    userObject.checks.push(checkId);
+                                                    lib.update('users',phone,userObject,(error)=>{
+                                                        if(error){
+                                                            callback(500,{
+                                                                error: "there was problem in server side."
+                                                            })
+                                                        }else{
+                                                            callback(200,checkObject);
+                                                        }
+                                                    })
+
                                                 }
                                             })
                                         }else{
@@ -89,7 +108,7 @@ handler._check.post = (requestProperties, callback) => {
                                         }
                                     }else{
                                         callback(403,{
-                                            error: "unauthenticated user !"
+                                            error: "unauthenticated user 1!"
                                         })
                                     }
                                 })
@@ -97,8 +116,8 @@ handler._check.post = (requestProperties, callback) => {
                         })
                     }else{
                         callback(500,{
-                            error : "there was a problem in server side !";
-                        }) 
+                            error : "there was a problem in server side !"
+                        });
                     }
                 }
             })
@@ -109,7 +128,7 @@ handler._check.post = (requestProperties, callback) => {
         }
     }else{
         callback(500,{
-            error : "there was a problem in server side !";
+            error : "there was a problem in server side !"
         })
     }
 
